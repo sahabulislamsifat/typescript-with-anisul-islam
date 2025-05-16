6a743339791dab6b4ad0e864172e8b5f36271309
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-17 01:30:33 +0600

    first commit

---
+++ first-program/index.js
@@ -0,0 +1,28 @@
+// let userName = "Sahabul Islam SIfat";
+var userName;
+// let userName: string;
+// userName = 123;
+// userName = "123";
+userName = true;
+// let userName = null;
+// let userName = undefined;
+console.log(userName);
+var userAge = 22;
+console.log(userAge);
+var userIsMarried = false;
+console.log(userIsMarried);
+var userHobbies = ["Reading", "Writing", "Coding"];
+console.log(userHobbies);
+var userAddress = {
+    city: "Dhaka",
+    country: "Bangladesh",
+};
+console.log(userAddress);
+var userDetails = {
+    name: "Sahabul Islam SIfat",
+    age: 22,
+    isMarried: false,
+    hobbies: ["Reading", "Writing", "Coding"],
+    address: { city: "Dhaka", country: "Bangladesh" },
+};
+console.log(userDetails);
