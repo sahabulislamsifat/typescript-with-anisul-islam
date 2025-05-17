dd50be2c99b33aa629747ee84a986c5f587f9425
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-17 14:29:30 +0600

    tuple types

---
+++ user-defined-type/tuple-types.js
@@ -0,0 +1,11 @@
+//* data / mixed / tuple-types.ts
+// This file contains the definition of a tuple type and a function that takes a tuple as an argument and returns its length.
+// The tuple type is defined as an array of elements of different types.
+//  let tuple: [number, string, boolean] = [1, "hello", true];
+var tuple;
+tuple = [1, "hello", true];
+console.log(tuple);
+tuple.push(2);
+tuple.push("world");
+tuple.push(false);
+console.log(tuple);
